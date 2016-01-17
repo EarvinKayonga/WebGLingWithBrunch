32c0f393be0c8b524b3177d20747cba0de7fb96d
EarvinKayonga <e.kayonga@bevolta.com>
2016-01-17 21:43:23 +0100

    Physi

--- public/index.js
+++ public/index.js
@@ -1,30 +1,69 @@
 var BigScene = (function() {
   "use strict";
 
-  var scene = new THREE.Scene(),
+  Physijs.scripts.worker = 'physi.js/physijs_worker.js';
+  Physijs.scripts.ammo = '/ammo.js/builds/ammo.js';
+
+  var scene = new Physijs.Scene(),
     renderer = new THREE.WebGLRenderer(),
     light = new THREE.AmbientLight(0xffffff),
     camera,
+    ground,
     box;
 
   function initScene() {
+    scene.setGravity(new THREE.Vector3(0, -50, 0));
+
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.getElementById("webgl-container").appendChild(renderer.domElement);
 
     scene.add(light);
-    camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.z = 100;
+    camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.set(60, 50, 60);
+    camera.lookAt(scene.position);
     scene.add(camera);
 
-    box = new THREE.Mesh(
-      new THREE.BoxGeometry(20, 20, 20),
+
+    var boxMaterial = Physijs.createMaterial(
       new THREE.MeshBasicMaterial({
         color: 0xFF0000
-      })
+      }),
+      0,
+      0.2
     );
+
+    box = new Physijs.BoxMesh(
+      new THREE.CubeGeometry(15, 15, 15),
+      boxMaterial
+    );
+
+    box.position.y = 40;
+    box.rotation.x = 45;
+    box.rotation.z = 45;
+    box.rotation.y = 45;
+
 
     box.name = "Hello World Box";
     scene.add(box);
+
+    var groundMaterial = Physijs.createMaterial(
+      new THREE.MeshBasicMaterial({
+        color: 0x008888
+      }),
+      0,
+      0
+    );
+
+    ground = new Physijs.BoxMesh(
+      new THREE.CubeGeometry(150, 5, 150),
+      groundMaterial,
+      0
+    );
+
+    ground.name = 'ground';
+    ground.position.y =-25;
+    scene.add(ground);
+
 
     var stats = new Stats();
     stats.setMode(0);
@@ -43,11 +82,9 @@ var BigScene = (function() {
   }
 
   function render() {
-    box.rotation.y += 0.01;
-
+    scene.simulate();
     renderer.render(scene, camera);
     requestAnimationFrame(render);
-
   }
 
   window.onload = initScene;
